3e5cf5165e190fc7704694ea23f117c6982f49a8
yasat <ghantasat007@gmail.com>
2023-02-18 19:14:01 +0000

    Update computerfunction.py

--- computerfunction.py
+++ computerfunction.py
@@ -1,15 +1,5 @@
 def COMPUTERFUNCTION(x,y):
-  ans = ""
-  if(x%10 == y%10):
-    ans = str((x//10) + (y//10)) + str((x%10))
-  elif(x%10 > y%10):
-    a = x%10 - y%10
-    ans = str(a + (y//10)) + str(0)
-  elif(x%10 < y%10):
-    a = y%10 - x%10
-    ans = str((y//10) - a) + str(a)
-
-  return ans
+  return x ^ y
 
 print(COMPUTERFUNCTION(10 , 20))
 print(COMPUTERFUNCTION(17 , 35))
