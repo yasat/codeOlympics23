350ff578532c98d1806b3aea5ffc8b69bdbd1e8e
yasat <ghantasat007@gmail.com>
2023-02-18 15:51:41 +0000

    Create moorse.py

---
+++ moorse.py
@@ -0,0 +1,17 @@
+MORSE_CODE_DICT = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0'}
+
+moorse = input()
+
+text = ""
+
+moorse = moorse.replace("1",'.')
+moorse = moorse.replace("0",'-')
+
+words = moorse.split("  ")
+for word in words:
+  letters = word.split(" ")
+  for letter in letters:
+    text = text + MORSE_CODE_DICT[letter]
+  text = text + " "
+
+print(text)
